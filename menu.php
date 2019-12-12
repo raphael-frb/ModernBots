340af1196697e68e30a2e981b9cc92590eaac475
raphael-frb <54801271+raphael-frb@users.noreply.github.com>
2019-12-12 17:12:18 +0400

    Create menu.php

---
+++ menu.php
@@ -0,0 +1,30 @@
+<ul id="menu">
+        <li><a href="view_prod.php?cat=electromenager">Electroménagers</a>
+            <!--<ul>
+                <li><a href="view_prod.php?cat=electromenager">Machines à laver</a></li>
+                <li><a href="#">Réfrigérateurs</a></li>
+            </ul>-->
+        </li><li><a href="view_prod.php?cat=drone">Drones</a>
+            <!--<ul>
+                <li><a href="#">Level 2.1</a></li>
+                <li><a href="#">Level 2.2</a></li>
+            </ul>-->
+        </li><li><a href="view_prod.php?cat=telephone">Smartphones</a>
+            <!--<ul>
+                <li><a href="#">Level 3.1</a></li>
+                <li><a href="#">Level 3.2</a></li>
+            </ul>-->
+        </li><li><a href="view_prod.php?cat=montre">Montres</a>
+            <!--<ul>
+                <li><a href="#">Level 1.1</a></li>
+                <li><a href="#">Level 1.2</a></li>
+            </ul>-->
+        </li><li><a href="index.php?action=Deconnexion">Déconnexion</a></li>
+        <!-- Barre de recherche -->
+        <li>
+            <form action="/search" id="searchthis" method="POST">
+                <input id="search" name="q" type="text" placeholder="Entrez votre recherche" />
+                <input id="search-btn" type="submit" value="Rechercher" />
+            </form>
+        </li>
+</ul>
